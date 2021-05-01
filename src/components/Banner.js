6ef6704c5ecd14364310e6a085a43a1924c89b76
Muhammad Taha <taha_shahid@outlook.com>
2021-05-01 13:03:12 +0500

    sideNav fixed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
 import bannerImg from "../assets/images/banner.jpg";
+import M from "materialize-css";
 class Banner extends Component {
+  componentDidMount(){
+      var elems = document.querySelectorAll('.parallax');
+      M.Parallax.init(elems);
+    }
   render() {
     return (
       <div>
         <div className="parallax-container">
-            <p className="name">Nando's</p>
+            <p style={stylebox.name}>Nando's</p>
           <div className="parallax">
             <img src={bannerImg} alt="banner"/>
           </div>
         </div>
       </div>
     );
+  }
+}
+const stylebox = {
+  name: {
+    fontFamily: 'Pacifico, cursive',
+    textAlign: 'center',
+    opacity:' 0.8',
+    textTransform: 'uppercase',
+    color: 'white',
+    fontWeight: 500,
+    fontSize: 70,
+    background: 'rgba(100,100,100,0.5)',
+    borderColor:' rgb(100, 100, 100)',
+    marginTop: '6%',
   }
 }
 export default Banner;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,48 +1,57 @@
 import React, { Component } from "react";
 import logo from "../assets/images/logo.jpg";
 import "../App.css";
+import M from 'materialize-css'
 class navbar extends Component {
   constructor(props) {
     super(props);
     this.state = { background: "black", color: "white" };
   }
+  componentDidMount(){
+    var elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems);
+  }
   render() {
     return (
-      <div>
+      <>
         <nav style={{ backgroundColor: this.state.background }}>
           <div className="nav-wrapper">
+          <a href="#" data-target="mobile-demo" class="right sidenav-trigger"><i class="material-icons">menu</i></a>
             <a
               href="!#"
               className="left brand-logo"
               style={{ paddingLeft: 20, color: this.state.color }}
             >
-              <img className="circle" src={logo} alt="logo" />
+              <img style={styleBox.circle} src={logo} alt="logo" />
             </a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
               <li>
-                <a href="!#" className="bold-text">
+                <a href="!#" style={styleBox.boldtext}>
                   Home
                 </a>
               </li>
               <li>
-                <a href="!#" className="bold-text">
+                <a href="!#" style={styleBox.boldtext}>
                   About Us
                 </a>
               </li>
               <li>
-                <a href="!#" className="bold-text">
+                <a href="!#" style={styleBox.boldtext}>
                   Menu
                 </a>
               </li>
               <li>
-                <a href="!#" className="bold-text">
+                <a href="!#" style={styleBox.boldtext}>
                   Contact
                 </a>
               </li>
               <li>
-                <a href="!#" className="waves-effect waves-green btn-small red bold">
-                  <i className="material-icons right ">local_grocery_store</i>Order
-                  Now
+                <a
+                  href="!#"
+                  className="waves-effect waves-light btn-small red bold" 
+                >
+                  <i className="material-icons right ">local_grocery_store</i>
+                  Order Now
                 </a>
               </li>
               {/* <li>
@@ -60,8 +69,27 @@ class navbar extends Component {
             </ul>
           </div>
         </nav>
-      </div>
+        <ul className="sidenav" id="mobile-demo">
+          <li><a href="#top">Home</a></li>
+          <li><a href="#aboutus">About us</a></li>
+          <li><a href="#appeteasers">Menu</a></li>
+          <li><a href="#aboutus">Contacts</a></li>
+          <li>
+            <i className="fa fa-facebook"  style={{fontSize: 24 , paddingRight: 20, paddingLeft: 20}}></i>
+            <i className="fa fa-instagram" style={{fontSize: 24 , paddingRight:20, paddingLeft: 20}}></i>
+            <i className="fa fa-twitter	"  style={{fontSize: 24 , paddingRight: 20, paddingLeft: 20}}></i>
+          </li>
+        </ul>
+      </>
     );
   }
+}
+const styleBox = {
+  boldtext:{
+    fontWeight: 'bold'
+  },
+  circle:{
+    borderRadius: '50%'
+}
 }
 export default navbar;
