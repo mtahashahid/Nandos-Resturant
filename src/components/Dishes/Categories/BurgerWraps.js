edfdafd22ffe9f062c360811ad78b288f05875fa
Muhammad Taha <taha_shahid@outlook.com>
2021-05-12 23:11:23 +0500

    burger component added

---
+++ src/components/Dishes/Categories/BurgerWraps.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class BurgerWraps extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default BurgerWraps;
